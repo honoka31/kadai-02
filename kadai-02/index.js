442217c590502251c504476ea6bb0bed67174eb6
honoka31 <honoka31@users.noreply.github.com>
2021-10-20 04:55:14 +0000

    課題2作成

---
+++ kadai-02/index.js
@@ -0,0 +1,13 @@
+const firstName = "hono"
+const familyName = "hono"
+console.log(firstName + familyName)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const dog = false
+const cat = true
+console.log(dog || cat)//犬または猫を飼っているかどうか
+console.log(dog && cat)//犬と猫の両方を飼っている
+console.log(!dog && !cat)//犬も猫も飼っていない
+console.log(!(dog || cat))
