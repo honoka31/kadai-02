53fda5690e493d33a43f9ed5e6570f613add5502
honoka31 <honoka31@users.noreply.github.com>
2021-11-26 21:04:39 +0000

    課題6を作成

---
+++ kadai-06/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs")
+const data = fs.readFileSync("./kadai-06/data.json").toString("utf-8")
+try{
+    const obj = JSON.parse(data)
+    obj.updateAt = Date()
+    obj.count = obj.count + 1
+    fs.writeFileSync("./kadai-06/data.json", JSON.stringify(obj))
+    console.log("正常に終了しました。")
+} catch(error){
+    console.log("不正な JSON フォーマットです")
+}
